9cc71d96d96e19858bab457d80e7be118e2749d1
Boris Belyaev <bbor1974@gmail.com>
2025-06-27 16:02:19 +0400

    Use kernel and modules from fog2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,6 +70,6 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 TW_BRIGHTNESS_PATH := /sys/class/disp/disp/attr/lcd_bl
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_LANGUAGE := ru
-TW_DEVICE_VERSION := by bbor74 
+TW_DEVICE_VERSION := by bbor74(kb2)
 
 
